853befb0cac2e719446fb68091621a665bd4ed01
rademuth <rad9aw@virginia.edu>
2015-10-20 16:26:10 -0400

    Added a new scene with a Map Kit View and added it to the existing navigation controller.

---
+++ Shellder/MapViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MapViewController.swift
+//  Shellder
+//
+//  Created by Robert Demuth on 10/20/15.
+//  Copyright © 2015 CS 4720. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    
+    // MARK: Properties
+    @IBOutlet weak var mapView: MKMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
